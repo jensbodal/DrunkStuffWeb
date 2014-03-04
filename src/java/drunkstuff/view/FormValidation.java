b8601ec683f1b207d12bd71b3017c92c26d932f0
Jens Bodal <jensbodal@gmail.com>
2014-03-03 17:19:12 -0800

    Added JSTL library from http://repo1.maven.org/maven2/jstl/jstl/1.2/jstl-1.2.jar

--- src/java/drunkstuff/view/FormValidation.java
+++ src/java/drunkstuff/view/FormValidation.java
@@ -8,13 +8,10 @@ package drunkstuff.view;
 
 import drunkstuff.calculator.Person;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
